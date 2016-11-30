e01c265b6f1eca70f2629a5cf7f00a4dfc815a8c
Zareh <z.zareh@gmail.com>
2016-11-30 11:21:17 +0100

    GoogleMap eingebunden

--- app/src/main/java/com/example/monkeyman/rundgangdurchdiealtstadt/MapsActivity.java
+++ app/src/main/java/com/example/monkeyman/rundgangdurchdiealtstadt/MapsActivity.java
@@ -1,36 +1,38 @@
 package com.example.monkeyman.rundgangdurchdiealtstadt;
 
+import android.app.Activity;
+import android.graphics.Camera;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-
+    CameraPosition camera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        setContentView(R.layout.activity_main);
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map_fragment);
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        googleMap.setMyLocationEnabled(true);
+        LatLng schaerding = new LatLng(48.456754, 13.431395);
+        camera = CameraPosition.builder().target(schaerding).zoom(16).build();
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleMap.addMarker(new MarkerOptions().position(schaerding).title("Marker in Schärding"));
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(camera));
     }
 }
