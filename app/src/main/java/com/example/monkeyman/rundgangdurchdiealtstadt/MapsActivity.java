f38dce3813479b12438a35e3200e979b22b43e4e
DArminger <davidarminger@gmx.at>
2017-02-17 11:32:47 +0100

    Preferences

--- app/src/main/java/com/example/monkeyman/rundgangdurchdiealtstadt/MapsActivity.java
+++ app/src/main/java/com/example/monkeyman/rundgangdurchdiealtstadt/MapsActivity.java
@@ -5,6 +5,7 @@ import android.Manifest;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -23,6 +24,8 @@ import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
@@ -31,7 +34,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.os.EnvironmentCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -66,6 +72,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class MapsActivity extends FragmentActivity
         implements OnMapReadyCallback,
@@ -79,6 +87,64 @@ public class MapsActivity extends FragmentActivity
     GoogleApiClient googleApiClient;
     Location lastLocation;
     LocationManager locationManager;
+    SharedPreferences s = new SharedPreferences() {
+        @Override
+        public Map<String, ?> getAll() {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public String getString(String s, String s1) {
+            return null;
+        }
+
+        @Nullable
+        @Override
+        public Set<String> getStringSet(String s, Set<String> set) {
+            return null;
+        }
+
+        @Override
+        public int getInt(String s, int i) {
+            return 0;
+        }
+
+        @Override
+        public long getLong(String s, long l) {
+            return 0;
+        }
+
+        @Override
+        public float getFloat(String s, float v) {
+            return 0;
+        }
+
+        @Override
+        public boolean getBoolean(String s, boolean b) {
+            return false;
+        }
+
+        @Override
+        public boolean contains(String s) {
+            return false;
+        }
+
+        @Override
+        public Editor edit() {
+            return null;
+        }
+
+        @Override
+        public void registerOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener onSharedPreferenceChangeListener) {
+
+        }
+
+        @Override
+        public void unregisterOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener onSharedPreferenceChangeListener) {
+
+        }
+    };
 
     private LocationRequest locationRequest;
     private final int UPDATE_INTERVAL = 3 * 60 * 1000;
@@ -246,6 +312,22 @@ public class MapsActivity extends FragmentActivity
                     .radius(GEOFENCE_RADIUS);
             geoFenceLimits = googleMap.addCircle(circleOptions);
             markers.put(m, s);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.optionsmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        switch(id){
+            case R.id.optionen: startActivity(new Intent(this, PrefsActivity.class));
+                return true;
+            default: return false;
         }
     }
 
